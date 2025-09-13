d7ba241e592f75230f48366bdbdd9a4fc3f7c9ba
oyinmons123 <oyinkan776@gmail.com>
2025-09-13 11:33:45 +0100

    initial commit: React app

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import './App.css'
+import About from './components/About'
+import Service from './components/Service'
+import Projects from './components/Projects'  
+import Contact from './components/Contact'
+import Footer  from './components/Footer'
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Hero />
+    <About />
+    <Service />
+    <Projects />
+    <Contact />
+    <Footer />
+    </>
+  )
+}
+
+export default App
