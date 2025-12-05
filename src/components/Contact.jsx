60d6371ad9307bdddfd233d2100fd5554c003ab3
Yusuf Monsurat <oyinkan776@gmail.com>
2025-12-05 14:48:49 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
-import {FaEnvelope, FaMapMarkedAlt, FaPhone} from "react-icons/fa"
+import { FaEnvelope, FaMapMarkedAlt, FaPhone } from "react-icons/fa"
 
 const Contact = () => {
-    return(
+    return (
         <div className="bg-black text-white py-20" id="contact">
             <div className="container mx-auto px-8 md:px-16 lg:px-24">
                 <h2 className="text-4xl font-bold text-center mb-12">Contact</h2>
@@ -10,50 +10,50 @@ const Contact = () => {
                         <h3 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 mb-4">Let's Talk</h3>
                         <p>I'm open to discussing web development projects or partnership opportunities</p>
                         <div className="mb-4 mt-8">
-                            <FaEnvelope className="inline-block text-green-400 mr-2"/>
+                            <FaEnvelope className="inline-block text-green-400 mr-2" />
                             <a href="oyinkan776@gmail.com" className="hover:underline">
                                 oyinkan776@gmail.com
                             </a>
                         </div>
-                         <div className="mb-4">
-                            <FaPhone className="inline-block text-green-400 mr-2"/>
-                           <span>+2348128714940</span>
+                        <div className="mb-4">
+                            <FaPhone className="inline-block text-green-400 mr-2" />
+                            <span>+2348128714940</span>
                         </div>
-                         <div className="mb-4">
-                            <FaMapMarkedAlt className="inline-block text-green-400 mr-2"/>
+                        <div className="mb-4">
+                            <FaMapMarkedAlt className="inline-block text-green-400 mr-2" />
                             <span>
                                 Asadam, Ilorin, Kwara, Nigeria
                             </span>
-                         </div>
+                        </div>
                     </div>
                     <div className="flex-1 w-full">
                         <form className="space-y-4">
                             <div>
-                                <label htmlFor="name">Your Name </label>
-                                <input type="text" 
-                                className="w-full p-2 rounded bg-gray-800 focus:outline-none focus:border-green-400"
-                                placeholder="Enter Your Name:"/>
+                                <label htmlFor="name" className="block mb-2">Your Name </label>
+                                <input type="text"
+                                    className="w-full p-2 rounded bg-gray-800 focus:outline-none focus:border-green-400"
+                                    placeholder="Enter Your Name:" />
                             </div>
                             <div>
                                 <label htmlFor="email" className="block mb-2">Email</label>
-                                <input type="text" 
-                                className="w-full p-2 rounded bg-gray-800 focus:outline-none focus:border-green-400"
-                                placeholder="Enter Your Email"/>
+                                <input type="text"
+                                    className="w-full p-2 rounded bg-gray-800 focus:outline-none focus:border-green-400"
+                                    placeholder="Enter Your Email" />
                             </div>
-                              <div>
+                            <div>
                                 <label htmlFor="email" className="block mb-2">Message</label>
-                                <textarea type="text" 
-                                className="w-full p-2 rounded bg-gray-800 focus:outline-none focus:border-green-400"
-                                rows={5}
-                                placeholder="Enter Your Message"/>
+                                <textarea type="text"
+                                    className="w-full p-2 rounded bg-gray-800 focus:outline-none focus:border-green-400"
+                                    rows={5}
+                                    placeholder="Enter Your Message" />
                             </div>
                             <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">Send</button>
                         </form>
                     </div>
                 </div>
             </div>
-        </div>       
+        </div>
     )
 }
-    
+
 export default Contact; 
